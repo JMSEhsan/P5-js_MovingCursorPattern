dcb7348222ef1ca6ebc85dd91161fa7c6f4bdb14
Ehsan Jalali <JMSEhsan@gmail.com>
2022-08-28 14:34:12 -0600

    commit: changes made on coloring

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 
 // Source: https://p5js.org/examples/drawing-patterns.html
+// I made some changes with regard to coloring
 
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(1200, 600);
   background(118, 243, 231);
 }
 
@@ -20,6 +21,8 @@ function draw() {
 
 function variableEllipse(x, y, px, py) {
   let speed = abs(x - px) + abs(y - py);
-  stroke(speed);
+  stroke(speed*Math.random(), speed*Math.random(), speed*Math.random());
+  fill(speed*Math.random()*100, speed*Math.random()*100, speed*Math.random()*100);
   ellipse(x, y, speed, speed);
 }
+
